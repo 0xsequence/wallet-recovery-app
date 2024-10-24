d0c3c362626aff3ac643809af148bcafd09da2f6
William Hua <william@attente.ca>
2024-10-23 22:59:31 -0400

    arweave config reader

--- src/utils/tracker.ts
+++ src/utils/tracker.ts
@@ -1,27 +1,5 @@
 import { trackers } from '@0xsequence/sessions'
-import { ethers } from 'ethers'
 
-import { DEFAULT_PUBLIC_RPC_LIST } from '~/constants/network'
-import { SEQUENCE_CONTEXT } from '~/constants/wallet-context'
-
-// TODO: remove once network work is done, this should use rpc from NetworkStore.networks
-const polygonRpcUrl = DEFAULT_PUBLIC_RPC_LIST.get(137)
-
-// TODO: temporary, remove/update once sessions work is done
-export const LOCAL_TRACKER = new trackers.DedupedTracker(
-  new trackers.local.LocalConfigTracker(
-    new ethers.JsonRpcProvider(polygonRpcUrl),
-    new trackers.stores.IndexedDBStore('local-tracker-v1.0.1')
-  ),
-  50
-)
-
-// TODO: temporary, remove/update once sessions work is done
-export const TRACKER = new trackers.DedupedTracker(
-  new trackers.CachedTracker(
-    new trackers.DedupedTracker(new trackers.remote.RemoteConfigTracker('https://sessions.sequence.app'), 50),
-    LOCAL_TRACKER,
-    SEQUENCE_CONTEXT
-  ),
-  50
-)
+export const TRACKER = new trackers.arweave.ArweaveReader({
+  graphqlUrl: 'https://arweave-search.goldsky.com/graphql'
+})
