5a4feea93dc390220549945a4b8448261487710e
attente <william@attente.ca>
2024-11-05 16:52:27 +0000

    Build: (c68a7e8) add remove buttons on user added tokens and collectibles

--- assets/index-C5NmdHMJ.js
+++ assets/index-C5NmdHMJ.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-CMNaN0uD.js";import"./index-xFq7-ZOq.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-BSsWHlTg.js";import"./index-CuFYxWjS.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
