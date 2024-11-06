a2eb901805820d059c39614ff4a7e6a102089420
pkieltyka <pkieltyka@users.noreply.github.com>
2024-11-06 03:15:46 +0000

    Build: (18f43dc) update router base name (#13)

--- assets/index-kOrb-30k.js
+++ assets/index-kOrb-30k.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-SPESv6LF.js";import"./index-zdST_U5z.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-v5Uez4kt.js";import"./index-K-bAd4dG.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
