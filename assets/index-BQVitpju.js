7965b477895b3d07d7b5ffe953a2539b636bc425
pkieltyka <pkieltyka@users.noreply.github.com>
2024-11-06 03:08:41 +0000

    Build: (dc06766) Merge pull request #12 from 0xsequence/update-vite-base
    update vite base and gh-pages action

--- assets/index-BQVitpju.js
+++ assets/index-BQVitpju.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-BSEof6bf.js";import"./index-B10cr2A7.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-SPESv6LF.js";import"./index-zdST_U5z.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
