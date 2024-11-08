ff6c093dfbd1011784e5274805a1c2d94169c419
attente <william@attente.ca>
2024-11-08 05:20:01 +0000

    Build: (04e94b1) remove settings token list

--- assets/index-B2EN6X9B.js
+++ assets/index-B2EN6X9B.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-ofr9Efhr.js";import"./index-DfUk3RqI.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-CNW4lHpP.js";import"./index-BYj-0w-s.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
