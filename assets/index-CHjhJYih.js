e91804ad650a069a61c06e1ae7df47037e24440d
attente <william@attente.ca>
2024-11-08 05:45:16 +0000

    Build: (56682e4) remove dropdown settings menu

--- assets/index-CHjhJYih.js
+++ assets/index-CHjhJYih.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-CNW4lHpP.js";import"./index-BYj-0w-s.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-PiiJGyIz.js";import"./index-BNFziPFK.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
