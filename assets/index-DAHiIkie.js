b406299005fc500d4a6020771e779379eb817275
tolgahan-arikan <tolgahan.arikan@gmail.com>
2024-12-03 17:33:40 +0000

    Build: (e464820) Ux feedback pass (#43)
    * refactored paths to absolute paths, changed recovery logo
    
    * fixed browser bug where wallet page is not centered
    
    * changed font size for default cards on wallet page
    
    * WIP: change font sizes
    
    * fixed header spacing
    
    * changed text font weight and color to match figma
    
    * fixed font weight doubles
    
    * updated collectible item to match figma
    
    * added mobile popover for space optimization
    
    * changed mobile popover icons to white
    
    * changed mobile nav drawer to match builder
    
    * added closing of nav when networks modal opens
    
    * removed unused delcaration
    
    * added mobile styling to landing page
    
    * Changed styling for mobile, fixed recovering wallet button bug
    
    * Added padding on bottom

--- assets/index-DAHiIkie.js
+++ assets/index-DAHiIkie.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-BTRILIvE.js";import"./index-BjDBzBW_.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-B1sGzfvq.js";import"./index-DZan3ykS.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
