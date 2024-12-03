0d23e8713b0036180b39c56c2ecec80e62a8f4aa
tolgahan-arikan <tolgahan.arikan@gmail.com>
2024-12-03 17:50:27 +0000

    Build: (17f6ba8) fixed desktop logo styling (#47)

--- assets/index-D0hVQxpf.js
+++ assets/index-D0hVQxpf.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-DktADLpu.js";import"./index-Cfjx0TSH.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-ClTLqRAP.js";import"./index-DWMRO5WV.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
