03473a2b6cda4c206dde6d0a3667121603d71b78
tolgahan-arikan <tolgahan.arikan@gmail.com>
2024-12-03 17:45:51 +0000

    Build: (f24911d) fixed desktop styling breaking (#46)

--- assets/index-CeuvvY4b.js
+++ assets/index-CeuvvY4b.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-B1sGzfvq.js";import"./index-DZan3ykS.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-DktADLpu.js";import"./index-Cfjx0TSH.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
