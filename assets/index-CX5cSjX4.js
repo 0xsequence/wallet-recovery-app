5b004383e4fc583356f5021ba3fe8b3d8b9b9d04
tolgahan-arikan <tolgahan.arikan@gmail.com>
2024-12-04 14:44:23 +0000

    Build: (d7148a8) fixed padding around password prompt on desktop (#48)

--- assets/index-CX5cSjX4.js
+++ assets/index-CX5cSjX4.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-ClTLqRAP.js";import"./index-DWMRO5WV.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-C7cIX-gi.js";import"./index-MwfxBU-o.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
