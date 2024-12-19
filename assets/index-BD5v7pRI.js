bbbf472a01e0e7545ad004476fa0bbc78e76f0ec
tolgahan-arikan <tolgahan.arikan@gmail.com>
2024-12-19 14:27:46 +0000

    Build: (d8cbb55) improved styling and added confirmation for resetting token list (#50)

--- assets/index-BD5v7pRI.js
+++ assets/index-BD5v7pRI.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-A4Zsexr-.js";import"./index-CTGKqe1u.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-D2FGXmIL.js";import"./index-CNZxwrDg.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
